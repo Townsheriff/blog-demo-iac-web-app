af8f02d5db1895e3c0f72b42bc2343f2f01af896
Arturs Valenieks <arturs@zumvie.com>
2023-05-20 15:34:28 +0300

    Add docker files and compose

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
 
 const app = express();
+const port = 3000;
 
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.send("Hello World From Backend");
 });
 
-app.listen(3001, () => {
-  console.log("Server started on port 3001");
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,29 +1,73 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  entry: "./src/index.tsx",
-  mode: "production",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "build"),
-    clean: true,
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js", ".jsx"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
+module.exports = (_, args) => {
+  if (args.mode === "production") {
+    return createProductionConfig();
+  }
+
+  return createDevelopmentConfig({
+    devServerPort: 4000,
+  });
+};
+
+const createDevelopmentConfig = (config) => {
+  return {
+    entry: "./src/index.tsx",
+    mode: "development",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "build"),
+      clean: true,
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js", ".jsx"],
+    },
+    devServer: {
+      port: config.devServerPort,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, "public", "index.html"),
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public", "index.html"),
-    }),
-  ],
+  };
+};
+
+const createProductionConfig = () => {
+  return {
+    entry: "./src/index.tsx",
+    mode: "production",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "build"),
+      clean: true,
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js", ".jsx"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, "public", "index.html"),
+      }),
+    ],
+  };
 };
